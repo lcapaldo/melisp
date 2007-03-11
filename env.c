1edb00105d095a09dc56076f8645f1d4f9eb9bc8
Logan Capaldo <logancapaldo@gmail.com>
2007-03-11 05:56:34 -0400

    Got some environment action going

--- env.c
+++ env.c
@@ -1,11 +1,9 @@
 #include "env.h"
 #include "types.h"
 #include "list.h"
-#include "print.h"
-#include <stdio.h>
 static struct mel_value* lookup_r(struct mel_pool* p, struct mel_value* name, struct mel_value* env);
 struct mel_value* mel_lookup(struct mel_pool* p, struct mel_value* name, struct mel_value* env) {
-  return lookup_r(p, name, env);
+  return lookup_r(p, name, mel_car( env ));
 }
 
 
@@ -23,11 +21,13 @@ static struct mel_value* lookup_r(struct mel_pool* p, struct mel_value* name, st
 
 
 struct mel_value* mel_set_global(struct mel_pool* p, struct mel_value* name, struct mel_value* val, struct mel_value* env) {
-  return mel_cons(p, mel_cons(p, name, val ), env);
+  env->value.pair_val.fst = mel_cons(p, mel_cons(p, name, val ), mel_car( env ));
+  return env;
 }
 
 struct mel_value* mel_standard_env(struct mel_pool* p) {
-  return mel_set_global(p, mel_cdr(mel_read(p, "bar")), mel_alloc_float(p, 2.3), mel_set_global(p, mel_cdr(mel_read(p, "foo" ) ), mel_alloc_int(p, 7), 0));
+  struct mel_value* env = mel_cons(p, 0, 0);
+  return mel_set_global(p, mel_cdr(mel_read(p, "bar")), mel_alloc_float(p, 2.3), mel_set_global(p, mel_cdr(mel_read(p, "foo" ) ), mel_alloc_int(p, 7), env));
 }
 
 
--- eval.c
+++ eval.c
@@ -18,7 +18,7 @@ struct mel_value* mel_eval(struct mel_pool* p, struct mel_value* expr, struct me
     case mel_strt:
       return expr;
     case mel_pairt:
-      if ( is_special_form(p, mel_car(expr) ) ) {
+      if ( is_special_form(p, expr) ) {
         return eval_special_form(p, expr, env );
       } else {
         return eval_function_call(p, expr, env );
@@ -31,14 +31,22 @@ struct mel_value* mel_eval(struct mel_pool* p, struct mel_value* expr, struct me
 static struct mel_value* eval_special_form(struct mel_pool* p, struct mel_value* expr, struct mel_value* env) { 
 
   if( mel_sym_eq( mel_car( expr ), mel_cdr(mel_read(p, "set")) ) ) {
-    env = mel_set_global( p, mel_car( mel_cdr( expr ) ), mel_eval(p,mel_car( mel_cdr( mel_cdr( expr ))), env), env);
+    struct mel_value* binding;
+    struct mel_value* val;
+    binding = mel_car( mel_cdr( expr ) );
+    val = mel_eval(p,  mel_car( mel_cdr( mel_cdr( expr ) ) ), env);
+    mel_set_global(p, binding, val, env);
+    return val; 
   }
+  return 0;
 }
 
 static struct mel_value* eval_function_call(struct mel_pool* p, struct mel_value* expr, struct mel_value* env) {  return 0; }
 static int is_special_form(struct mel_value* p, struct mel_value* expr ) {
-  if( mel_sym_eq( expr->value.pair_val.fst , mel_cdr( mel_read(p, "set")) ) ) {
+  if( mel_sym_eq( mel_car( expr ), mel_cdr(mel_read(p, "set"))) ) {
     return 1;
+  } else {
+    return 0;
   }
 }
 
