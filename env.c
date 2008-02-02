2c9767a97d316159aae6a2d8ad1a2bd328d79472
Logan Capaldo <logancapaldo@gmail.com>
2008-02-01 19:52:30 -0500

    Added 3 lines of lisp, deleted a bunch of C

--- env.c
+++ env.c
@@ -13,10 +13,8 @@ static struct mel_value* mul( struct mel_pool* p, struct mel_value* args);
 static struct mel_value* cons(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* car(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* cdr(struct mel_pool* p, struct mel_value* args);
-static struct mel_value* string_to_list(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* eval(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* list(struct mel_pool* p, struct mel_value* args);
-static struct mel_value* id(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* obj_count(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* byte_count(struct mel_pool* p, struct mel_value* args);
 static struct mel_value* print_memory(struct mel_pool* p, struct mel_value* args);
@@ -72,11 +70,8 @@ struct mel_value* mel_standard_env(struct mel_pool* p) {
   mel_set_global(p, mel_cdr(mel_read(p, "cons")), mel_alloc_cfun(p, cons));
   mel_set_global(p, mel_cdr(mel_read(p, "car")), mel_alloc_cfun(p, car));
   mel_set_global(p, mel_cdr(mel_read(p, "cdr")), mel_alloc_cfun(p, cdr));
-  mel_set_global(p, mel_cdr(mel_read(p, "string->list")), mel_alloc_cfun(p, string_to_list));
-  mel_set_global(p, mel_cdr(mel_read(p, "copy-list")), mel_alloc_cfun(p, string_to_list));
   mel_set_global(p, mel_cdr(mel_read(p, "eval")), mel_alloc_cfun(p, eval));
   mel_set_global(p, mel_cdr(mel_read(p, "list")), mel_alloc_cfun(p, list));
-  mel_set_global(p, mel_cdr(mel_read(p, "id")), mel_alloc_cfun(p, id));
   mel_set_global(p, mel_cdr(mel_read(p, "object-count")), mel_alloc_cfun(p, obj_count));
   mel_set_global(p, mel_cdr(mel_read(p, "print-memory")), mel_alloc_cfun(p, print_memory));
   mel_set_global(p, mel_cdr(mel_read(p, "print")), mel_alloc_cfun(p, print));
@@ -194,15 +189,6 @@ static struct mel_value* car(struct mel_pool* p, struct mel_value* args) {
 static struct mel_value* cdr(struct mel_pool* p, struct mel_value* args) {
   return mel_cdr( mel_car( args ) );
 }
-static struct mel_value* string_to_list1( struct mel_pool* p, struct mel_value* arg ) {
-  if( arg == 0 ) { return 0; }
-  return mel_cons(p, mel_car( arg ), string_to_list1(p, mel_cdr( arg )));
-}
-
-static struct mel_value* string_to_list(struct mel_pool* p, struct mel_value* args) {
-  struct mel_value* arg = mel_car( args );
-  return mel_cons(p, mel_car( arg ), string_to_list1(p, mel_cdr( arg ) ));
-}
 
 static struct mel_value* eval(struct mel_pool* p, struct mel_value* args) {
   return mel_eval(p, mel_car( args ));
@@ -210,10 +196,6 @@ static struct mel_value* eval(struct mel_pool* p, struct mel_value* args) {
 
 static struct mel_value* list(struct mel_pool* p, struct mel_value* args) {
   return args;
-}
-
-static struct mel_value* id(struct mel_pool* p, struct mel_value* args) {
-  return mel_car( args );
 }
 
 static struct mel_value* obj_count(struct mel_pool* p, struct mel_value* args) {
